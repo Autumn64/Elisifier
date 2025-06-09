dfe811dcb5d936b15eefb5aa0b3252a8b5e805cf
Mónica Nayely Flores Gómez <autumn64@disroot.org>
2025-06-08 23:39:21 -0600

    Improvements
    Signed-off-by: Mónica Nayely Flores Gómez <autumn64@disroot.org>

--- src/downloader.py
+++ src/downloader.py
@@ -1,18 +1,19 @@
-# Elisifier; A free online music downloader
-# Copyright (C) 2025  Mónica Gómez (Autumn64)
+""" Elisifier; A free online music downloader
+Copyright (C) 2025  Mónica Gómez (Autumn64)
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import os
 import re
@@ -35,57 +36,60 @@ class Downloader():
         self.rmscript = rmscript # Ruta del script que limpiará los archivos
         self.uuid = str(uuid.uuid4()) # ID único para la descarga actual
     
+    def getUUID(self) -> str:
     # Getter del UUID
-    def obtenerUUID(self) -> str:
         return self.uuid
 
+    def updateProgress(self, value: float, text: str):
     # Actualiza la barra de progreso de la descarga
-    def actualizarProgreso(self, value: float, text: str):
         self.progress.progress(value, text=text)
 
+    def updateStatus(self, msg: str, state: str):
     # Agrega los mensajes capturados de STDOUT al st.status
-    def actualizarStatus(self, msg: str, state: str):
         self.status.write(msg)
         self.status.update(label=msg, state=state)
     
+    def runDownload(self):
     # Ejecuta el script `download.sh` y captura STDOUT para ponerlo en Streamlit
-    def ejecutarDescarga(self):
         # Desempaqueta la lista de las urls
         ydl = subprocess.Popen(stdout=subprocess.PIPE, text=True, args=[self.script, self.uuid, self.fmt, *self.url])
 
         while (line := ydl.stdout.readline()) != "":
-            linea = line.rstrip()
+            dLine = line.rstrip()
             # Comprobación si no se pudo descargar ningún archivo
-            if "FATAL ERROR: No audio files were downloaded." in linea:
+            if "FATAL ERROR: No audio files were downloaded." in dLine:
                 st.session_state['dstate'] = "failed"
-                self.eliminarTodo(False)
+                self.removeAll(False)
                 return
 
             # Si una o más canciones no se pudieron descargar, notifica a Streamlit y continúa
-            if "ERROR:" in linea and st.session_state['derrors'] == False:
+            if "ERROR:" in dLine and st.session_state['derrors'] == False:
                 st.session_state['derrors'] = True
 
-            if "Downloading item " in linea:
-                regex = re.search(r'item (\d+) of (\d+)', linea)
+            if "Downloading item " in dLine:
+                regex = re.search(r'item (\d+) of (\d+)', dLine)
                 n1 = regex.group(1); n2 = regex.group(2)
-                self.actualizarProgreso((float(n1) / float(n2) ), f"Descargando canción {n1} de {n2}...")
+                self.updateProgress((float(n1) / float(n2) ), f"Descargando canción {n1} de {n2}...")
 
-            self.actualizarStatus(linea, "running")
+            self.updateStatus(dLine, "running")
 
-            if "Done." in linea:
-                self.actualizarStatus(linea, "complete")
-                self.actualizarProgreso(100, "Procesando...")
-                self.crearZip()
+            if "Adding cover" in dLine:
+                self.updateProgress(0, "Procesando, por favor espere...")
+
+            if "Done." in dLine:
+                self.updateStatus("Generando archivo .zip", "complete")
+                self.updateProgress(100, "Procesando, por favor espere...")
+                self.makeZip()
                 st.session_state['dstate'] = "finished"
-                self.eliminarTodo(True)
+                self.removeAll(True)
 
+    def makeZip(self):
     # Guarda la carpeta de descarga en un zip
-    def crearZip(self):
         shutil.make_archive(f"./download/{self.uuid}", "zip", f"./download/{self.uuid}")
-        self.actualizarStatus("Archivo listo para descarga.", "complete")
+        self.updateStatus("Archivo listo para descarga.", "complete")
 
+    def removeAll(self, run_script: bool):
     # Elimina la carpeta de antemano y ejecuta en modo detached el script que eliminará el zip después de un tiempo
-    def eliminarTodo(self, run_script: bool):
         shutil.rmtree(f"./download/{self.uuid}/")
         if run_script == False: return
         subprocess.Popen(
--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
-# Elisifier; A free online music downloader
-# Copyright (C) 2025  Mónica Gómez (Autumn64)
+""" Elisifier; A free online music downloader
+Copyright (C) 2025  Mónica Gómez (Autumn64)
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import os
 import json
@@ -33,9 +34,9 @@ from downloader import Downloader
 # pueda ser identificada por el programa en la URL, para que así reconozca que se trata de un link
 # de YouTube, y así proceda a realizar la descarga.
 
-# Cuadro de diálogo que aparece al dar al botón de "Ayuda"
 @st.dialog("Ayuda")
-def mensaje_ayuda():
+# Cuadro de diálogo que aparece al dar al botón de "Ayuda"
+def help_msg():
     st.markdown("### Pon una URL, da click a `Iniciar descarga`, y ¡disfruta!")
     st.markdown("**Elisifier** está basado en `yt-dlp`. Si bien este componente soporta [muchos](https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md) sitios web distintos, esta app sólo admite links de YouTube por razones de practicidad y complejidad.")
     st.markdown("Los links de Invidious y Piped funcionan (excepto las playlists, que esas no las lee), aunque es posible que alguna instancia no funcione si tiene una URL extraña.")
@@ -44,16 +45,16 @@ def mensaje_ayuda():
     st.divider()
     st.markdown("**Bajo licencia AGPL v3 o superior. [Ver código fuente](https://codeberg.org/Autumn64/Elisifier)**")
 
-# Callback de los botones
 def callback(value: bool):
+# Callback de los botones
     st.session_state['drunning'] = value
 
-# Muestra el título de la pantalla
 def banner():
+# Muestra el título de la pantalla
     st.markdown("<h2 style='text-align: center; '>Elisifier</h2><br><br>", unsafe_allow_html=True)
 
+def download_menu():
 # Función que prepara e inicia la descarga
-def menu_descarga():
     if st.session_state['dstate'] == "downloading":
         dProgress = st.progress(0, text="Descargando...")
         dStatus = st.status(label="Descargando. Puede tomar un rato...")
@@ -68,9 +69,9 @@ def menu_descarga():
             rmscript = "./rmcache.sh"
         )
 
-        st.session_state['did'] = downloader.obtenerUUID()
+        st.session_state['did'] = downloader.getUUID()
 
-        downloader.ejecutarDescarga()
+        downloader.runDownload()
         del downloader
 
         st.rerun()
@@ -84,9 +85,9 @@ def menu_descarga():
         st.session_state['dstate'] = 'idle'
         return
 
-    st.markdown("<h3 style='text-align: center; '>Canciones procesadas con éxito.</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; '>Canciones procesadas con éxito</h3>", unsafe_allow_html=True)
     if st.session_state['derrors'] == True:
-        st.markdown("<h5 style='text-align: center; '>Algunas canciones no se descargaron. Quizás tienen restricción de edad o no están disponibles.</h5><br>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: center; '>Algunas canciones no se descargaron. Quizás tienen restricción de edad o no están disponibles</h5><br>", unsafe_allow_html=True)
     
     # Botón de descarga basado en el UUID
     with open(f"./download/{dId}.zip", "rb") as f:
@@ -99,8 +100,8 @@ def menu_descarga():
             args=[False]
         )
 
+def main_menu():
 # Despliegue del menú principal
-def menu_principal():
     banner()
 
     # Comprobación de errores
@@ -108,23 +109,23 @@ def menu_principal():
         st.error("No se pudieron descargar las canciones. Refresque la página e inténtelo más tarde.")
         return
 
-    if st.columns([2.3, 1, 2.2])[1].button("Ayuda", disabled=st.session_state['drunning']): mensaje_ayuda()
+    if st.columns([2.3, 1, 2.2])[1].button("Ayuda", disabled=st.session_state['drunning']): help_msg()
 
     url = st.text_input(label="URL de playlists o de videos separadas por espacio", placeholder="https://music.youtube.com/...", disabled=st.session_state['drunning'])
     
     format_combo = st.columns([1.3, 1, 1.5])[1].selectbox(label="Formato", 
                                 options=["opus", "ogg", "flac", "mp3", "m4a"], accept_new_options=False, disabled=st.session_state['drunning'])
 
-    sButton = st.columns([1.5, 1, 1.5])[1].button("Iniciar descarga", disabled=st.session_state['drunning'], on_click=callback, args=[True])
+    sd_button = st.columns([1.5, 1, 1.5])[1].button("Iniciar descarga", disabled=st.session_state['drunning'], on_click=callback, args=[True])
 
     # Pone en el session_state `urlerror` para saber que streamlit debe mostrar un mensaje de error.
     # Sólo bloquea las URLs si la configuración está activada.
-    if sButton and not any(x in url for x in config['accepted']) and config['block_foreign_urls'] == True:
+    if sd_button and not any(x in url for x in config['accepted']) and config['block_foreign_urls'] == True:
         st.session_state['dstate'] = "urlerror"
         callback(False)
         st.rerun()
     
-    if sButton:
+    if sd_button:
         st.session_state['derrors'] = False
         st.session_state['dfmt'] = format_combo
         st.session_state['durl'] = url
@@ -134,7 +135,7 @@ def menu_principal():
         st.error("La URL ingresada no es válida o no es de YouTube.")
     
     if st.session_state['dstate'] in ['downloading', 'finished']:
-        menu_descarga()
+        download_menu()
 
 if __name__ == "__main__":
     # Carga de configuración
@@ -157,4 +158,4 @@ if __name__ == "__main__":
     if 'drunning' not in st.session_state:
         st.session_state['drunning'] = False
     
-    menu_principal()
+    main_menu()
